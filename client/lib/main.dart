17503d70c4b1227bc43c0ac373773255fcedf287
Daniel Ferraz <dxferraz@gmail.com>
2022-03-09 20:16:03 -0300

    fix android compilation bugs

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:humane/Services/service_locator.dart';
-import 'app.dart'; 
+import 'app.dart';
 
 Future<void> main() async {
   // TODO: Create cmd to change env on package.json
